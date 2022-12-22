ba83096a12e91ffa954cba174239d22138ae52e7
Stogov Anton <sloppybait@yandex.ru>
2022-12-22 16:23:37 +0300

    Initial Commit

---
+++ View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  View
+//
+//  Created by Антон on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
